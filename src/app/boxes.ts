9b0db00c7dc3fd720f174bb6aff78e7abf59a170
mzh <ma.hed@bluewin.ch>
2018-07-24 15:29:20 +0200

    Updated layout

--- src/app/boxes.ts
+++ src/app/boxes.ts
@@ -1,5 +1,27 @@
 export let boxes = [
-    {title: 'Sitehaus'},
+    {
+        title: 'Sitehaus',
+        text: 'Eine Webseite setzt sich zusammen aus Gestaltung, Interaktion und\
+        Technik. Um für den Besucher eine erfreuliche Erfahrung zu werden\
+        müssen diese Elemente ideal miteinander kombiniert werden.\
+        \
+        Wir sind professionelle Ingenieure die ausgehend von Ihrer\
+        industriellen Erfahrung wissen, wie eine effiziente und\
+        ansprechende Bedienung erfolgt.\
+        \
+        Diese Erfahrung setzen wir um in der Erstellung einer Webseite.'
+    },
+
+
+
+
+
+
+
+
+
+
+
     {title: 'Produktion'},
     {title: 'Technologie'}
 ];
